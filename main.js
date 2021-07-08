8552198e6ec28227a9ea5bebfe739a13121b03e4
DarkKillerXL <DarkKillerXL.DKXL@gmail.com>
2021-07-08 09:45:09 +0200

    Timer + Table Finished

--- main.js
+++ main.js
@@ -16,33 +16,55 @@ function logKey(e) {
 window.onkeydown = function(e) { 
     return !(e.keyCode == 32);
 };
+
+
 var start;
 var loop;
 var delta;
+var id = 0;
 
 function timer() {
     if (timerStatus == 0) {
         timerStatus = 1;
 
-        start = Date.now;
+        start = new Date();
         
         loop = setInterval(function () {
-            delta = Date.now() - start; // milliseconds elapsed since start
-        
-            console.log(delta); // in seconds
+            delta = new Date() - start; // milliseconds elapsed since start
 
-            document.getElementById('timer').innerHTML = toString(delta);
-        }, 1000); // update about every second
+            document.getElementById('timer').innerHTML = (delta/1000).toFixed(1);
+        }, 10); // update about every second
 
     } else {
         timerStatus = 0;
-
         clearInterval(loop);
+
+        document.getElementById('timer').innerHTML = (delta/1000).toFixed(2);
+        document.getElementById('table').innerHTML = document.getElementById('table').innerHTML + `<tr class='tr' id='${id}'>
+            <td>${document.getElementById('timer').innerHTML}</td>
+            <td>${document.getElementById('scramble').innerHTML}</td>
+            <td><i class="fas fa-times-circle" onclick="removeRow('${id}')"></i></td>
+        </tr>`;
+        id += 1;
+
+
+        generateScramble();
+
     }
 }
 
-function update() {
-
+function clearTable() {
+    var rows = document.getElementsByClassName('tr');
+    for (var i = 0, l = rows.length; i < l; i++) {
+        console.log(i)
+        rows[0].remove();
+        id = 0;
+    }
 };
 
-window.onload = timerStatus = 0; 
+function removeRow(id) {
+    document.getElementById(`${id}`).remove()
+}
+
+window.onload = timerStatus = 0;
+window.onload = table = document.getElementById('table'); 
